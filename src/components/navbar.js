96901c275b16c38f7016ac72bf6359c029a7ae6e
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-10-05 15:49:43 +0500

    Clean up and bootstrapped

---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="/">Ecommerce App</a>
+    </nav>
+  )
+}
