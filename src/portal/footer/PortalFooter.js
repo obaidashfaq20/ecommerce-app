6a8c85aa65175bb704002a86e93b1e22cffd9c13
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-11-14 16:38:36 +0500

    Add link to API documentation to the react footer

--- src/portal/footer/PortalFooter.js
+++ src/portal/footer/PortalFooter.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container } from 'react-bootstrap'
+import { BASE_URL } from '../../constants/constant'
 
 export default function PortalFooter() {
   return (
     <>
       <footer className='bg-light border-top py-3 fixed-bottom'>
-        <Container>
+        <Container className='d-flex justify-content-around'>
+          <div>
           &copy; React Auth Demo -2023 | Portal
+          </div>
+          <div>
+            <a href={ `${BASE_URL}/api-docs`} target="_blank">Go to API Docs</a>
+          </div>
         </Container>
       </footer>
     </>
