4adceae169e259eb6f904e31f4a89e1a89b25a06
obaidashfaq20 <obaidashfaq20@gmail.com>
2023-11-23 15:30:32 +0500

    Register order slice in store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import userReducer from "./features/user/userSlice";
 import productReducer from "./features/product/productSice";
 import settingReducer from "./features/setting/settingSlice";
 import cartSlice from "./features/cart/cartSlice";
+import orderSlice from "./features/order/orderSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     product: productReducer,
     setting: settingReducer,
-    cart: cartSlice
+    cart: cartSlice,
+    order: orderSlice
   },
 }); 
